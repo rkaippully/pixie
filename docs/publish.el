9f48eb6ec5b9eb6fc7ea528f4d5657c22a479bd9
Raghu Kaippully <rkaippully@gmail.com>
2021-02-17 19:23:39 +0530

    Added initial docs

---
+++ docs/publish.el
@@ -0,0 +1,13 @@
+(defun publish-project ()
+  (setq org-publish-project-alist
+        '(("pixie"
+           :base-directory "."
+           :publishing-directory "."
+           :publishing-function org-html-publish-to-html)))
+
+  (setq org-publish-use-timestamps-flag nil)
+
+  (org-publish-all))
+
+;; (publish-project)
+
